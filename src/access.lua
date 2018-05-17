a3f2a686049ff2e7c803b6ff262479f4cb0c6bc8
Mukesh Gupta <mukeshgupta.2006@gmail.com>
2018-05-17 09:59:13 +0530

    Update access.lua

--- src/access.lua
+++ src/access.lua
@@ -5,7 +5,7 @@ local _M = {}
 function _M.execute(plugin_conf)
   local u = url.parse(ngx.ctx.api.upstream_url)
   ngx.var.upstream_host=u.host  
-  ngx.ctx.balancer_address.host = plugin_conf.proxy_host
+  ngx.ctx.balancer_address.ip = plugin_conf.proxy_host
   ngx.ctx.balancer_address.port = plugin_conf.proxy_port 
 end
 
